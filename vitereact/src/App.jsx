a8b990f8e2b6ac82de3d985579189d5723142f43
114Abhijeet <abhijeetkatoria@gmail.com>
2024-06-11 13:19:26 +0530

    Understanding react flow

---
+++ vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+ //The tag <chai> is unrecognized in this browser. If you meant to render a React component, start its name with
+//  an uppercase letter.
+ import Chai from "./Chai";
+function App() {
+  return (
+    <>
+    <Chai/>
+    <h1>chai aur react</h1>
+    </>
+  )
+}
+export default App
---
+++ vitereact/src/Chai.jsx
@@ -0,0 +1,9 @@
+//if you write chai.js you will get error due to some naming convention in vite but it is totally fine in basicreact
+// chai.jsx is also accepted in basicreact
+// you can give file name to chai.jsx its fine.But best practice is to use Chai.jsx i.e First letter is Capital
+function Chai(){
+    return(
+        <h3>chai is ready</h3>
+    )
+}
+export default Chai;
